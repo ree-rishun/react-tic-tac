fd2c0331d0ebe1ebc7b8085ba5e35cba8c035c8d
ree-rishun <ree0432@gmail.com>
2021-02-14 05:17:53 +0900

    現在の位置をマーク

--- src/index.js
+++ src/index.js
@@ -95,7 +95,11 @@ class Game extends React.Component {
         'Start Over';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button
+            onClick={() => this.jumpTo(move)}
+            className={this.state.stepNumber === move ? "nowMove" : ""}
+          >
+            {desc}</button>
         </li>
       );
     });
